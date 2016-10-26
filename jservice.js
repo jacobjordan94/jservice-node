8716d5ae9d11e754ac045941ae6a08c298b07f84
jacobjordan94 <jacobjordan94@live.com>
2016-10-26 16:07:49 -0500

    initial commit

---
+++ jservice.js
@@ -0,0 +1,35 @@
+var request = require('request');
+var qs = require('query-string');
+
+class jService{
+	constructor(){
+		this.url = 'http://jservice.io/api/'
+	}
+
+	_makeRequest(){
+
+	}
+
+	clues(){
+
+	}
+
+	random(count, callback){
+		var url = 
+		count = count || 100;
+		count = count > 100 ? 100 : count;
+
+	}
+
+	categories(){
+
+	}
+
+	category(){
+
+	}
+
+	invalid(){
+
+	}
+}
